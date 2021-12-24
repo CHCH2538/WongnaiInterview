a8b3cc16983b39cfa10f3cc827652ba89466ae16
CHCH <49768100+CHCH2538@users.noreply.github.com>
2021-12-24 12:26:10 +0700

    Initial Commit

---
+++ Wongnai iOS Interview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Wongnai iOS Interview
+//
+//  Created by Pongpubate Charoensinphutthakhun on 24/12/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
